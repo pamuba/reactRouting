d4f906eab9a485e3f5ad5227c98d1654683287b7
pamuba <pamuba@users.noreply.github.com>
2020-07-22 21:52:05 +0530

    9

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button} from 'react-native';
-import { globalStyles } from '../styles/globals'
+import React from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles } from "../styles/globals";
+import Card from "../shared/card";
 
-export default function ReviewDetails({ navigation }){
-    
-    // const pressHandler = () =>{
-    //     // navigation.goBack();
-    //     navigation.pop();
-    // }
+export default function ReviewDetails({ navigation }) {
+  // const pressHandler = () =>{
+  //     // navigation.goBack();
+  //     navigation.pop();
+  // }
 
+  return (
+    <View style={globalStyles.container}>
+      <Card>
+        <Text>{navigation.getParam("title")}</Text>
+        <Text>{navigation.getParam("body")}</Text>
 
-
-    return(
-        <View style = {globalStyles.container}>
-            <Text>{navigation.getParam('title')}</Text>
-            <Text>{navigation.getParam('body')}</Text>
-            <Text>{navigation.getParam('rating')}</Text>
-            {/* <Button title='GO BACK TO HOME SCREEN' onPress={pressHandler}/> */}
+        <View stye={styles.rating}>
+          <Text>GameZone rating:</Text>
+          <Image source={require('../assets/rating-1.png')} />
         </View>
-    )
+
+        {/* <Text>{navigation.getParam('rating')}</Text> */}
+      </Card>
+      {/* <Button title='GO BACK TO HOME SCREEN' onPress={pressHandler}/> */}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+    //   container: {
+    //     padding: 24,
+    //   },
+});
+    

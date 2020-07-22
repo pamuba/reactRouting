eda183481e327a3ac134b385ca212f926ac3e6d8
pamuba <pamuba@users.noreply.github.com>
2020-07-22 21:23:49 +0530

    9

--- shared/header.js
+++ shared/header.js
@@ -2,10 +2,15 @@ import React from 'react'
 import {StyleSheet, View, Text} from 'react-native'
 import{ MaterialIcons } from '@expo/vector-icons'
 
-export default function Header(){
+export default function Header({navigation}){
+
+    const openMenu = () =>{
+        navigation.openDrawer()
+    }
+
     return(
         <View style={styles.header}>
-        {/* <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon} /> */}
+        <MaterialIcons name="menu" size={28} onPress={openMenu} style={styles.icon} />
             <View>
                 <Text style={styles.headerText}>GameZone</Text>
             </View>
@@ -19,7 +24,8 @@ const styles = StyleSheet.create({
        height: '100%',
        flexDirection:'row',
        alignItems:"center",
-       justifyContent:'center'
+       justifyContent:'center',
+    //    backgroundColor: 'red'
     }, 
     headerText:{
          fontWeight:'bold',
